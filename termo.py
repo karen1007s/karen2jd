f33b6eb1633ff291ac89c11a77329d3b106f3755
karen1007s <163157042+karen1007s@users.noreply.github.com>
2024-03-18 12:30:24 -0300

    Add files via upload

---
+++ termo.py
@@ -0,0 +1,9 @@
+def jogar() :
+    print("###### Bem vindo ao jogo da forca ######")
+acertou = False
+enforcou = False
+while (not acertou and not enforcou )
+     print("jogando")
+     print("##Fim do jogo##")
+    ## PROGRAMA O CRIADA
+    print("##### FIM DO JOGO #####")
